199a75a3fc538894a16fad4699a594bad263cb4a
Marcos Fernández Martínez <marcchef@gmail.com>
2014-10-04 03:33:22 +0200

    ComitInicial

---
+++ teams/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Team(models.Model):
+    name=models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Player(models.Model):
+    name=models.CharField(max_length=128)
+    team = models.ForeignKey(Team)
+
+
+    def __str__(self):
+        return '{}({})'.format(self.name,self.team)
--- urls.py
+++ urls.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'teamroulette.views.home', name='home'),
+    url(r'^$', 'teamroulette.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^test/$','teamroulette.views.test'),
+    #url(r'^home/$','teamroulette.views.home'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
